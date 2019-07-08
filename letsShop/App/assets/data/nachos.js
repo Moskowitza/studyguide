46199f06479506f37aa7482d1001157e4171b4d5
moskowitza <moskowitzaaron@mac.com>
2019-07-07 20:20:03 -0400

    directory permission errors

---
+++ letsShop/App/assets/data/nachos.js
@@ -0,0 +1,20 @@
+import uuid from 'uuid/v4';
+
+export default [
+  {
+    id: uuid(),
+    name: "Tofu"
+  },
+  {
+    id: uuid(),
+    name: "Seitan"
+  },
+  {
+    id: uuid(),
+    name: "Daiya"
+  },
+  {
+    id: uuid(),
+    name: "Beans"
+  }
+];
