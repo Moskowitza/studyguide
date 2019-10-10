ac888b18ca0151749b78f23f3ea0ef6708b4a5e4
moskowitza <moskowitzaaron@mac.com>
2019-10-10 17:22:58 -0400

    first

---
+++ studyguide/src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let name;
+  import Quiz from "./Quiz.svelte";
+</script>
+
+<style>
+  h1 {
+    color: purple;
+  }
+</style>
+
+<h1>Hello {name}!</h1>
+<Quiz />
---
+++ studyguide/src/Quiz.svelte
@@ -0,0 +1,22 @@
+<script>
+  let result = "answer is _";
+  let pickAnswer = a => {
+    result = `answer is ${a}`;
+  };
+</script>
+
+<style>
+  h1 {
+    color: red;
+    display: block;
+  }
+</style>
+
+<div>
+
+  <h4>&#8203 {result}</h4>
+  <button on:click={() => pickAnswer('a')}>A</button>
+  <button on:click={() => pickAnswer('b')}>B</button>
+  <button on:click={() => pickAnswer('c')}>C</button>
+  <button on:click={() => pickAnswer('d')}>D</button>
+</div>
