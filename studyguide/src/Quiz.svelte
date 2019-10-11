a61b1497f6f3136d1c540a6c3accc5f9fe2826de
moskowitza <moskowitzaaron@mac.com>
2019-10-11 09:47:34 -0400

    hit trivia api

--- studyguide/src/Quiz.svelte
+++ studyguide/src/Quiz.svelte
@@ -1,22 +1,43 @@
 <script>
-  let result = "answer is _";
-  let pickAnswer = a => {
-    result = `answer is ${a}`;
-  };
+  let result = null;
+  let answer = ["a", "b", "c", "d"];
+  let quiz = getQuiz();
+  let pickAnswer = a => {
+    result = `answer is ${a.toUpperCase()}`;
+  };
+  async function getQuiz() {
+    const res = await fetch(
+      "https://opentdb.com/api.php?amount=10&category=27&type=multiple"
+    );
+    const quiz = await res.json();
+    return quiz;
+  }
+  function handleClick() {
+    quiz = getQuiz();
+  }
 </script>
 
 <style>
-  h1 {
-    color: red;
-    display: block;
-  }
+  h4 {
+    color: red;
+    display: block;
+  }
 </style>
 
+<h1>Quiz</h1>
+<button on:click={handleClick}>Get Quiz</button>
+{#await quiz}loading{:then data}{data.results[0].question}{/await}
 <div>
+  {#if result}
+    <h4>{result}</h4>
+  {:else}
+    <h4>&#8203</h4>
+  {/if}
+  {#each answer as item}
+    <button on:click={() => pickAnswer(item)}>
+      Answer {item.toUpperCase()}
+    </button>
+  {/each}
 
-  <h4>&#8203 {result}</h4>
-  <button on:click={() => pickAnswer('a')}>A</button>
-  <button on:click={() => pickAnswer('b')}>B</button>
-  <button on:click={() => pickAnswer('c')}>C</button>
-  <button on:click={() => pickAnswer('d')}>D</button>
 </div>
+
